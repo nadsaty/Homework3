265516aaa0304772738cfd01e2a96cbda8e0a005
Усман <nadsatyyy@gmail.com>
2023-08-06 03:45:06 +0300

    Домашка 3 Задание 1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int firstOne = 1000000;
+        byte secondOne = 100;
+        short thirdOne = 10000;
+        long fourthOne = 1000000000;
+        float fifthOne = 1.1F;
+        double sixthOne = 1.111;
+        System.out.println("Значение переменной firstOne с типом int равно " + firstOne);
+        System.out.println("Значение переменной secondOne с типом byte равно " + secondOne);
+        System.out.println("Значение переменной thirdOne с типом short равно " + thirdOne);
+        System.out.println("Значение переменной fourthOne с типом long равно " + fourthOne);
+        System.out.println("Значение переменной fifthOne с типом float равно " + fifthOne);
+        System.out.println("Значение переменной sixthOne с типом double равно " + sixthOne);
+    }
+
+    public static void task2 () {
+    }
+}
